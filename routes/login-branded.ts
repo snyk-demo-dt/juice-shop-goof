949f414240e60eba9a9ca289874c35d84e63bcbd
shawnsnyk <38260497+shawnsnyk@users.noreply.github.com>
2025-03-17 16:41:38 -0400

    Update login-branded.ts

--- routes/login-branded.ts
+++ routes/login-branded.ts
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
+ * Sample update for PR demo
  */
 
 import models = require('../models/index')
